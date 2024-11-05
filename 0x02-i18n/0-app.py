cda88cd34f3268d35181744fc06794b62506a03e
Pury-01 <puryokumu@gmail.com>
2024-11-05 13:53:19 +0300

    corrected file name

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def index():
     """single route.
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
